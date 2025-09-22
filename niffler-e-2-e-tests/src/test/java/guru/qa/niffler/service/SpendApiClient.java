9be528b2b507023ca44a5d4bc731a79dbf9d6f20
Alexey <alexey.melnik@icloud.com>
2025-09-22 20:36:36 -0300

    First commit.
    Add Spend API integration and Firebase support
    
    - Implement SpendApi interface for creating spends.
    - Update SpendApiClient to use SpendApi for spend creation.
    - Modify SpendingExtension to utilize SpendApiClient instead of SpendDbClient.
    - Add Firebase configuration to .gitignore and package.json.
    - Introduce spendUrl method in Config and LocalConfig for API endpoint.

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendApiClient.java
@@ -1,16 +1,33 @@
 package guru.qa.niffler.service;
 
+import guru.qa.niffler.api.SpendApi;
+import guru.qa.niffler.config.Config;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
+import lombok.SneakyThrows;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.util.Optional;
 
 public class SpendApiClient implements SpendClient{
-  @Override
-  public SpendJson createSpend(SpendJson spend) {
-    throw new UnsupportedOperationException("Not implemented :(");
-  }
 
+    private static final Config CFG = Config.getInstance();
+
+    private final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(CFG.spendUrl())
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build();
+
+    private final SpendApi spendApi = retrofit.create(SpendApi.class);
+
+    @SneakyThrows
+    @Override
+    public SpendJson createSpend(SpendJson spend) {
+        return spendApi.createSpend(spend)
+                .execute()
+                .body();
+    }
   @Override
   public CategoryJson createCategory(CategoryJson category) {
     throw new UnsupportedOperationException("Not implemented :(");
