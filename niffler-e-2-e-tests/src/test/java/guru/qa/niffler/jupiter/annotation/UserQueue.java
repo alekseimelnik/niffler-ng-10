c095b5abd44ce2dfc9bc02d15c666df8bcda9448
Alexey <alexey.melnik@icloud.com>
2025-10-04 01:36:55 -0300

    Enhance FriendsPage and FriendsTest with Allure step annotations for better test reporting

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/UserQueue.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/UserQueue.java
@@ -1,13 +1,16 @@
 package guru.qa.niffler.jupiter.annotation;
 
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
 import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target({java.lang.annotation.ElementType.TYPE, java.lang.annotation.ElementType.METHOD})
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
 @ExtendWith(UsersQueueExtension.class)
 public @interface UserQueue {
 
