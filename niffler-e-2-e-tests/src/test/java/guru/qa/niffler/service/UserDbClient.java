5c9fbb12c22f5b47907beb008dc9552336b15b95
Alexey <alexey.melnik@icloud.com>
2025-10-20 11:19:17 -0300

    feat: refactor UserDaoJdbc and UserDbClient to use connection parameter for database operations

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/UserDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/UserDbClient.java
@@ -1,6 +1,8 @@
 package guru.qa.niffler.service;
 
-import guru.qa.niffler.data.dao.UserDao;
+import static guru.qa.niffler.data.Databases.transaction;
+
+import guru.qa.niffler.config.Config;
 import guru.qa.niffler.data.dao.impl.UserDaoJdbc;
 import guru.qa.niffler.data.entity.spend.UserEntity;
 import guru.qa.niffler.model.UserJson;
@@ -9,30 +11,46 @@ import java.util.UUID;
 
 public class UserDbClient implements UserClient {
 
-  private UserDao userDao = new UserDaoJdbc();
+  private static final Config CFG = Config.getInstance();
 
   @Override
   public UserJson createUser(UserJson user) {
-    UserEntity userEntity = UserEntity.fromJson(user);
-    return UserJson.fromEntity(
-        userDao.createUser(userEntity)
+    return transaction(connection -> {
+          UserEntity userEntity = UserEntity.fromJson(user);
+          return UserJson.fromEntity(
+              new UserDaoJdbc(connection).createUser(userEntity)
+          );
+        },
+        CFG.spendJdbcUrl()
     );
   }
 
   @Override
   public Optional<UserJson> findById(UUID id) {
-    return userDao.findById(id)
-        .map(UserJson::fromEntity);
+    return transaction(connection -> {
+          return new UserDaoJdbc(connection).findById(id)
+              .map(UserJson::fromEntity);
+        },
+        CFG.spendJdbcUrl()
+    );
   }
 
   @Override
   public Optional<UserJson> findByUsername(String username) {
-    return userDao.findByUsername(username)
-        .map(UserJson::fromEntity);
+    return transaction(connection -> {
+          return new UserDaoJdbc(connection).findByUsername(username)
+              .map(UserJson::fromEntity);
+        },
+        CFG.spendJdbcUrl()
+    );
   }
 
   @Override
   public void delete(UserJson user) {
-    userDao.delete(UserEntity.fromJson(user));
+    transaction(connection -> {
+          new UserDaoJdbc(connection).delete(UserEntity.fromJson(user));
+        },
+        CFG.spendJdbcUrl()
+    );
   }
 }
