6ccc70a99e96f1fea941d8697830de9d3d30807c
Alexey <alexey.melnik@icloud.com>
2025-10-20 23:41:04 -0300

    feat: add Authority model and related JSON representations

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/model/auth/UserAuthJson.java
@@ -0,0 +1,26 @@
+package guru.qa.niffler.model.auth;
+
+import guru.qa.niffler.data.entity.auth.UserAuthEntity;
+import java.util.UUID;
+
+public record UserAuthJson(
+    UUID id,
+    String username,
+    String password,
+    Boolean enabled,
+    Boolean accountNonExpired,
+    Boolean accountNonLocked,
+    Boolean credentialsNonExpired
+) {
+    public static UserAuthJson fromEntity(UserAuthEntity entity) {
+        return new UserAuthJson(
+            entity.getId(),
+            entity.getUsername(),
+            entity.getPassword(),
+            entity.getEnabled(),
+            entity.getAccountNonExpired(),
+            entity.getAccountNonLocked(),
+            entity.getCredentialsNonExpired()
+        );
+    }
+}
